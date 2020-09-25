f8fde44478b53a402d157c45e2245afaae136652
Tanweer.Mehdi <Tanweer.Mehdi@bentley.com>
2020-09-26 00:22:11 +0530

    First Commit

---
+++ XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+using System;
+using TestPipeline.Controllers;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class UnitTest1
+    {
+
+        WeatherForecastController weatherct = new WeatherForecastController();
+        [Fact]
+        public void Test1()
+        {
+            var returnValue = weatherct.Get();
+            Assert.Equal("Tanweer", returnValue);
+        }
+       
+    }
+}
